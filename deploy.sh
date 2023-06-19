7025a602e9334a6440c62bf285b9689271019da0
Lisandro <lisandro.marina@bweventstech.com>
2023-06-18 21:40:23 -0400

    add bash to deploy

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,13 @@ echo "Running Maven build..."
 # Build the Maven project
 mvn clean package
 
+# Check the exit status of the Maven build
+maven_exit_status=$?
+if [ $maven_exit_status -ne 0 ]; then
+  echo "Maven build failed. Exiting..."
+  exit $maven_exit_status
+fi
+
 echo "Running docker compose..."
 # Run Docker Compose
 docker-compose up -d
