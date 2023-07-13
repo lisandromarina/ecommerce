74b3c1de2ad50e7efbe865c99e47f547c0dc2d9a
Lisandro <lisandro.marina@bweventstech.com>
2023-07-13 10:04:12 -0400

    allowed origin

--- src/main/java/com/ecommerce/ECommerceApplication.java
+++ src/main/java/com/ecommerce/ECommerceApplication.java
@@ -17,7 +17,7 @@ public class ECommerceApplication {
 			return new WebMvcConfigurer() {
 				@Override
 				public void addCorsMappings(CorsRegistry registry) {
-					registry.addMapping("/**").allowedOrigins("http://localhost:3000").allowedMethods("*").allowedHeaders("*");
+					registry.addMapping("/**").allowedOrigins("*").allowedMethods("*").allowedHeaders("*");
 				}
 			};
 		}
