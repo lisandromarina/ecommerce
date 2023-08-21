c5774876e15eda1bbf0d2a42939b728d4d29c513
lisandromarina <lisandromarina1@gmail.com>
2023-08-20 21:15:05 -0400

    Separate Create and update product
    -change controller response
    -change service
    -change service imp

--- src/main/java/com/ecommerce/controller/ShoppingCartProductController.java
+++ src/main/java/com/ecommerce/controller/ShoppingCartProductController.java
@@ -1,8 +1,11 @@
 package com.ecommerce.controller;
 
 import com.ecommerce.DTO.ShoppingCartProductDTO;
+import com.ecommerce.model.ShoppingCartProduct;
+import com.ecommerce.service.ShoppingCartProductService;
 import com.ecommerce.service.impl.ShoppingCartProductServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -10,11 +13,16 @@ import org.springframework.web.bind.annotation.*;
 public class ShoppingCartProductController {
 
     @Autowired
-    ShoppingCartProductServiceImpl shoppingCartProductServiceImpl;
+    ShoppingCartProductService shoppingCartProductServiceImpl;
 
     @PostMapping("/create")
-    public void createOrUpdatehoppingCartProduct(@RequestBody ShoppingCartProductDTO shoppingCartProductDTO) {
-        shoppingCartProductServiceImpl.createOrUpdate(shoppingCartProductDTO);
+    public ResponseEntity<ShoppingCartProduct> createShoppingCartProduct(@RequestBody ShoppingCartProductDTO shoppingCartProductDTO) {
+        return shoppingCartProductServiceImpl.createShoppingCartProduct(shoppingCartProductDTO);
+    }
+
+    @PatchMapping("/update")
+    public  ResponseEntity<ShoppingCartProduct> updateShoppingCartProduct(@RequestBody ShoppingCartProductDTO shoppingCartProductDTO) {
+        return shoppingCartProductServiceImpl.updateShoppingCartProduct(shoppingCartProductDTO);
     }
 
     @DeleteMapping("/delete/{shoppingCartId}/{productId}")
--- src/main/java/com/ecommerce/service/ShoppingCartProductService.java
+++ src/main/java/com/ecommerce/service/ShoppingCartProductService.java
@@ -1,14 +1,18 @@
 package com.ecommerce.service;
 
 import com.ecommerce.DTO.ShoppingCartProductDTO;
+import com.ecommerce.model.ShoppingCartProduct;
+import org.springframework.http.ResponseEntity;
 
 import java.util.Set;
 
 public interface ShoppingCartProductService {
 
-    void createOrUpdate(ShoppingCartProductDTO shoppingCartProductDTO);
-
     void delete(Long shoppingCartId, Long productId);
 
     Set<ShoppingCartProductDTO> findByShoppingCartId(Long id);
+
+    ResponseEntity<ShoppingCartProduct> updateShoppingCartProduct(ShoppingCartProductDTO shoppingCartProductDTO);
+
+    ResponseEntity<ShoppingCartProduct> createShoppingCartProduct(ShoppingCartProductDTO shoppingCartProductDTO);
 }
